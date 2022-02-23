80ebe3944207a1adb6e21c97d817e3e853bd03a4
Shivam Tandon <tandonshivam05@gmail.com>
2022-02-23 11:08:15 +0530

    checking

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { login } from "../../context/authContext/apiCalls";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/authContext/AuthContext";
 import "./login.scss";
 
@@ -40,7 +41,10 @@ export default function Login() {
             Sign In
           </button>
           <span>
-            New to Netflix? <b>Sign up now.</b>
+            New to Netflix? 
+            <Link to="/register" style={{"z-index":"999"}} className="link">
+            <b>Sign up now.</b>
+          </Link>
           </span>
           <small>
             This page is protected by Google reCAPTCHA to ensure you're not a
